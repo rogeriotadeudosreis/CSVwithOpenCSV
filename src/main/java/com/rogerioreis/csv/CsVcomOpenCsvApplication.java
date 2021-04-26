6630a52588bb07fdf1a0ad5a896064d84a0a6064
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr@hotmail.com>
2021-04-26 15:29:16 -0300

    init repository

---
+++ src/main/java/com/rogerioreis/csv/CsVcomOpenCsvApplication.java
@@ -0,0 +1,13 @@
+package com.rogerioreis.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsVcomOpenCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsVcomOpenCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogerioreis/csv/CsVcomOpenCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerioreis.csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsVcomOpenCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
